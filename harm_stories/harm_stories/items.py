32f7f9134b83af3d079ee6b0f2dc2557649b8413
Lidiane Taquehara <lidi.mayra@gmail.com>
2018-04-14 16:47:47 -0300

    Add Harm Stories Item
    An item to specify which fields a harm story might have.

--- harm_stories/harm_stories/items.py
+++ harm_stories/harm_stories/items.py
@@ -5,10 +5,12 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class HarmStoriesItem(scrapy.Item):
+class HarmStoriesItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    topic = Field()
+    harmed_people = Field()
+
